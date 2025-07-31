29e6c817e2b7fe045b2de822f53e755a804a5139
Josh <joshbell635@gmail.com>
2025-07-30 17:30:19 -0700

    Adjusted sessions controller

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/itemsController');
+const controller = require('../controllers/sessionsController');
 
 router.post('/', controller.createSession);
 
